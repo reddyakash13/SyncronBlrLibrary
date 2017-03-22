957e74b757c7f910528571b61a475711614b8a88
Akash Reddy <akash.reddy@syncron.com>
2017-03-22 11:48:06 +0530

    Startup page with dummy modal pop up
    Change-Id: Ia713d8b94bd988f4af9a43cb8574f9e10ab2e22c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,28 @@ import { AppComponent } from './app.component';
 import { BookShelfComponent } from './book-shelf.component';
 import { ListViewComponent } from './listview.component';
 import { TitleComponent } from './title.component';
+import { BootstrapModalModule } from 'angular2-modal/plugins/bootstrap';
+import { ModalModule } from 'angular2-modal';
+
+import { CustomModal } from './custom-modal-sample';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookShelfComponent,
     ListViewComponent,
-    TitleComponent
+    TitleComponent,
+    CustomModal
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BootstrapModalModule,
+    ModalModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [CustomModal]
 })
 export class AppModule { }
--- src/app/book-shelf.component.ts
+++ src/app/book-shelf.component.ts
@@ -1,23 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from './book';
 import { BookService } from './book.service';
+import { Modal, BSModalContext } from 'angular2-modal/plugins/bootstrap';
+import { Overlay, overlayConfigFactory } from 'angular2-modal';
+import { CustomModalContext, CustomModal } from './custom-modal-sample';
 
 @Component({
   selector: 'app-book-shelf',
   templateUrl: './book-shelf.component.html',
   styleUrls: ['./book-shelf.component.css'],
-  providers: [BookService]
+  providers: [BookService, Modal]
 })
 export class BookShelfComponent implements OnInit {
   books: Book[];
 
-  constructor(private bookService: BookService) {}
+  constructor(private bookService: BookService, public modal: Modal) { }
 
   ngOnInit(): void {
     this.getBooks();
   }
 
   getBooks(): void {
-     this.bookService.getBooks().then(books => this.books = books);
+     this.bookService.getDummyBooks().subscribe(books => this.books = books);
   }
+
+  openCustom(): void {
+    this.modal.alert()
+        .size('lg')
+        .showClose(true)
+        .title('A simple Alert style modal window')
+        .body(`
+            <h4>Alert is a classic (title/body/footer) 1 button modal window that
+            does not block.</h4>
+            <b>Configuration:</b>
+            <ul>
+                <li>Non blocking (click anywhere outside to dismiss)</li>
+                <li>Size large</li>
+                <li>Dismissed with default keyboard key (ESC)</li>
+                <li>Close wth button click</li>
+                <li>HTML content</li>
+            </ul>`)
+        .open();
+  }
+
+  onClick() {
+    return this.modal.open(CustomModal,  overlayConfigFactory({ num1: 2, num2: 3 }, BSModalContext));
+  }
+
 }
